6ba914965edf1aff54498217e8a5c5af18745dff
Mitesh <ravindramitesh@gmail.com>
2020-08-31 18:47:23 -0400

    final commit before deployment

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,8 @@ var socket = require("socket.io");
 
 // App setup
 var app = express();
-var server = app.listen(3000, function () {
-  console.log("listening for requests on port 3000,");
+var server = app.listen(3001, function () {
+  console.log("listening for requests on port 3001,");
 });
 
 // Socket setup & pass server
@@ -12,6 +12,7 @@ var io = socket(server);
 io.on("connection", (socket) => {
   // Handle chat event
   socket.on("chat", function (data) {
+    console.log(data);
     // Broadcast the data to every client
     io.emit("broadcast", data);
   });
